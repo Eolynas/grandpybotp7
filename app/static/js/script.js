afbc88b55e00c376eb77975a1c77fb14a3ae190b
Eddy <eddy.hubert@yahoo.fr>
2021-01-18 12:02:58 +0100

    edit script js pour mettre message de chargement avant la réponse

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -46,14 +46,30 @@ $(document).ready(function () {
             data: $('form').serialize(),
             dataType: 'json',
             beforeSend: function (data) {
+                var loading_message = "\
+                            <div class='d-flex justify-content-start mb-4' id='message_bot_loading'>\
+                                <div class='img_cont_msg' >\
+                                    <img id='message_bot' alt='avatar' src='/static/img/grandpybot.jpg' class='rounded-circle user_img_msg'>\
+                                </div>\
+                                    <div class='loading-msg'> \
+                                        <div class='msg_cotainer'>\
+                                            <div class='canvas canvas6'>\
+                                                <div class='spinner p1'></div>\
+                                                <div class='spinner p2'></div>\
+                                                <div class='spinner p3'></div>\
+                                                <div class='spinner p4'></div>\
+                                            </div>\
+                                        </div>\
+                                    </div>\
+                            </div>"
                 $("#message").val("");
+                $('#message-chatbot').append(loading_message)
             },
             success: function (data) {
                 console.log(data)
                 console.log(data.data.address)
-                var message = "Voici l'adresse de " + data.data.message + ": " + data.data.address
-                $('#message-chatbot').append("\
-                            <div class='d-flex justify-content-start mb-4'>\
+                var message = "Voici l'adresse de " + data.data.message.data + ": " + data.data.address
+                var dom_message_bot = "<div class='d-flex justify-content-start mb-4'>\
                                     <div class='img_cont_msg'>\
                                         <img id='message_bot' alt='avatar' src='/static/img/grandpybot.jpg' class='rounded-circle user_img_msg'>\
                                     </div>\
@@ -62,13 +78,13 @@ $(document).ready(function () {
                                         <span class='msg_time'>"+ data.data.message.date +"</span>\
                                     </div>\
                                 </div>"
-                )
-            }
+                setTimeout(function(){
+                    $('#message_bot_loading').remove()
+                    $('#message-chatbot').append(dom_message_bot)
+                }, 2000);
+            },
 
         });
-        // setTimeout(function(){
-        //         $("#message_bot").replaceWith(<p id='message_bot'> Voici ma réponse </p>)
-        //         }, 2000);
 
 
     });
