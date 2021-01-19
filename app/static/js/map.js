a843e1ee0a413486268d581aab857c85965eb8d2
Eddy <eddy.hubert@yahoo.fr>
2021-01-19 06:28:11 +0100

    fin de journée

---
+++ app/static/js/map.js
@@ -0,0 +1,16 @@
+// let map;
+//
+// function initMap() {
+//   // "lat": 47.9879489,
+//   // "lng": 0.2084077
+//   const myLatLng = { lat: 47.9879489, lng: 0.2084077 };
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 12,
+//     center: myLatLng,
+//   });
+//   new google.maps.Marker({
+//     position: myLatLng,
+//     map,
+//     title: "Hello World!",
+//   });
+// }
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,13 +4,13 @@ $(document).ready(function () {
         var url = 'message';
         e.preventDefault();
         const str_message = $('#message').val();
-                if (str_message){
-                    const now = new Date();
-                    const date_now = now.getDate() + '/' + now.getMonth() + '/' + now.getFullYear() + ' ' + now.getHours() + ':' + now.getMinutes();
-                    console.log('val ' + str_message)
+        if (str_message) {
+            const now = new Date();
+            const date_now = now.getDate() + '/' + now.getMonth() + '/' + now.getFullYear() + ' ' + now.getHours() + ':' + now.getMinutes();
+            console.log('val ' + str_message)
 
 
-                    $('#message-chatbot').append("\
+            $('#message-chatbot').append("\
                         <div class='d-flex justify-content-end mb-4'>\
                                 <div class='msg_cotainer_send'>\
                                     " + str_message + "\
@@ -21,25 +21,25 @@ $(document).ready(function () {
                                 </div>\
                         </div>")
 
-                    // setTimeout(function(){
-                    //     $('#message-chatbot').append("\
-                    //         <div class='d-flex justify-content-start mb-4'>\
-                    //             <div class='img_cont_msg' >\
-                    //                 <img id='message_bot' alt='avatar' src='/static/img/grandpybot.jpg' class='rounded-circle user_img_msg'>\
-                    //             </div>\
-                    //                 <div class='loading-msg'> \
-                    //                     <div class='msg_cotainer'>\
-                    //                         <div class='canvas canvas6'>\
-                    //                             <div class='spinner p1'></div>\
-                    //                             <div class='spinner p2'></div>\
-                    //                             <div class='spinner p3'></div>\
-                    //                             <div class='spinner p4'></div>\
-                    //                         </div>\
-                    //                     </div>\
-                    //                 </div>\
-                    //         </div>")
-                    // }, 2000);
-                    }
+            // setTimeout(function(){
+            //     $('#message-chatbot').append("\
+            //         <div class='d-flex justify-content-start mb-4'>\
+            //             <div class='img_cont_msg' >\
+            //                 <img id='message_bot' alt='avatar' src='/static/img/grandpybot.jpg' class='rounded-circle user_img_msg'>\
+            //             </div>\
+            //                 <div class='loading-msg'> \
+            //                     <div class='msg_cotainer'>\
+            //                         <div class='canvas canvas6'>\
+            //                             <div class='spinner p1'></div>\
+            //                             <div class='spinner p2'></div>\
+            //                             <div class='spinner p3'></div>\
+            //                             <div class='spinner p4'></div>\
+            //                         </div>\
+            //                     </div>\
+            //                 </div>\
+            //         </div>")
+            // }, 2000);
+        }
         $.ajax({
             type: "POST",
             url: url,
@@ -61,27 +61,35 @@ $(document).ready(function () {
                                             </div>\
                                         </div>\
                                     </div>\
-                            </div>"
+                            </div>\
+                            <div id='map' class='map'></div>"
                 $("#message").val("");
                 $('#message-chatbot').append(loading_message)
+
+
             },
             success: function (data) {
                 console.log(data)
                 console.log(data.data.address)
                 var message = "Voici l'adresse de " + data.data.message.data + ": " + data.data.address
-                var dom_message_bot = "<div class='d-flex justify-content-start mb-4'>\
-                                    <div class='img_cont_msg'>\
-                                        <img id='message_bot' alt='avatar' src='/static/img/grandpybot.jpg' class='rounded-circle user_img_msg'>\
-                                    </div>\
-                                    <div class='msg_cotainer'>\
-                                        "+ message +"\
-                                        <span class='msg_time'>"+ data.data.message.date +"</span>\
-                                    </div>\
-                                </div>"
-                setTimeout(function(){
+                var dom_message_bot = "\
+                                <div class='d-flex justify-content-start mb-4'>\
+                                        <div class='img_cont_msg'>\
+                                                <img id='message_bot' alt='avatar' src='/static/img/grandpybot.jpg' class='rounded-circle user_img_msg'>\
+                                        </div>\
+                                        <div class='msg_cotainer'>\
+                                                " + message + "\
+                                                <span class='msg_time'>" + data.data.message.date + "</span>\
+                                        </div>\
+                                </div>\
+                                <div id='map' class='map'></div>"
+
+                setTimeout(function () {
                     $('#message_bot_loading').remove()
                     $('#message-chatbot').append(dom_message_bot)
+                    initMap()
                 }, 500);
+
             },
 
         });
@@ -90,3 +98,20 @@ $(document).ready(function () {
     });
 
 });
+
+let map;
+
+function initMap() {
+    // "lat": 47.9879489,
+    // "lng": 0.2084077
+    const myLatLng = {lat: 47.9879489, lng: 0.2084077};
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: myLatLng,
+    });
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "Hello World!",
+    });
+}
