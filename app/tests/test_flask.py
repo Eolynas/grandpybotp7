f68ba3ddbe548b76e965fa5f87206f6e8ea21d86
Eddy <eddy.hubert@yahoo.fr>
2021-01-25 10:02:53 +0100

    creation mock for unit test

--- app/tests/test_flask.py
+++ app/tests/test_flask.py
@@ -25,39 +25,6 @@ class TestFlaskApp:
         res = self.client.get('/chatbot')
         assert res.status_code == 200
 
-    # def test_post_form_index(self):
-    #     """
-    #     simulate post form
-    #     :return:
-    #     """
-    #     data_form = 'Chatbot'
-    #     c = self.client.post('/', data={'name_project': data_form})
-    #     # print(c.data)
-    #     assert c.status_code == 200
-    #     assert data_form in str(c.data)
-
-    # CREER LE MESSAGE ET L'ENVOIE EN POST SUR LA ROUTE 'chatbot'
-    # def test_post_chatbot(self):
-    #     """
-    #     # TODO A FAIRE
-    #     :return:
-    #     """
-    #     data_form = "Bonjour, connnait tu l'adresse d'Openclassrooms"
-    #     c = self.client.post('/message', data={'message': data_form})
-    #     response = self.client.post(
-    #         '/message',
-    #         data={'message': data_form},
-    #         # content_type='application/json',
-    #     )
-    #     # print(c.data)
-    #     assert c.status_code == 200
-    #     assert response.status_code == 200
-    #     # print(response.get_data())
-    #     data = json.loads(response.get_data(as_text=True))
-    #     # assert data['user'] == 'Eddy'
-    #     print(data['data'][0]['message'])
-    #     assert data_form in data['data'][0]['message']
-
     def test_parser(self):
         """
         # TODO A FAIRE
@@ -87,6 +54,34 @@ class TestFlaskApp:
         # parse_message = parser.parser(messages_speciaux[1])
         # assert parse_message == ["bonjour"]
 
+    def mock_api_google(self, search: str) -> dict:
+        txt_input = "openclassrooms"
+        request_no_found = {
+            "results": [],
+            "status": "ZERO_RESULTS"
+        }
+        request_found = {
+            "results": [
+                {
+                    "formatted_address": "10 Quai de la Charente, 75019 Paris, France",
+                    "geometry": {
+                        "location": {
+                            "lat": 48.8975156,
+                            "lng": 2.3833993
+                        },
+                    },
+                },
+            ],
+            "status": "OK"
+        }
+
+        if search.lower() == txt_input.lower():
+            return request_found
+        elif search == "":
+            return request_no_found
+        else:
+            return request_no_found
+
     def test_get_api_google(self):
         """
         # TODO A FAIRE
@@ -95,89 +90,14 @@ class TestFlaskApp:
         example_true_reponse_api = {
             "results": [
                 {
-                    "address_components": [
-                        {
-                            "long_name": "10",
-                            "short_name": "10",
-                            "types": [
-                                "street_number"
-                            ]
-                        },
-                        {
-                            "long_name": "Quai de la Charente",
-                            "short_name": "Quai de la Charente",
-                            "types": [
-                                "route"
-                            ]
-                        },
-                        {
-                            "long_name": "Paris",
-                            "short_name": "Paris",
-                            "types": [
-                                "locality",
-                                "political"
-                            ]
-                        },
-                        {
-                            "long_name": "Département de Paris",
-                            "short_name": "Département de Paris",
-                            "types": [
-                                "administrative_area_level_2",
-                                "political"
-                            ]
-                        },
-                        {
-                            "long_name": "Île-de-France",
-                            "short_name": "IDF",
-                            "types": [
-                                "administrative_area_level_1",
-                                "political"
-                            ]
-                        },
-                        {
-                            "long_name": "France",
-                            "short_name": "FR",
-                            "types": [
-                                "country",
-                                "political"
-                            ]
-                        },
-                        {
-                            "long_name": "75019",
-                            "short_name": "75019",
-                            "types": [
-                                "postal_code"
-                            ]
-                        }
-                    ],
                     "formatted_address": "10 Quai de la Charente, 75019 Paris, France",
                     "geometry": {
                         "location": {
                             "lat": 48.8975156,
                             "lng": 2.3833993
                         },
-                        "location_type": "ROOFTOP",
-                        "viewport": {
-                            "northeast": {
-                                "lat": 48.8988645802915,
-                                "lng": 2.384748280291502
-                            },
-                            "southwest": {
-                                "lat": 48.8961666197085,
-                                "lng": 2.382050319708498
-                            }
-                        }
                     },
-                    "place_id": "ChIJIZX8lhRu5kcRGwYk8Ce3Vc8",
-                    "plus_code": {
-                        "compound_code": "V9XM+29 Paris, France",
-                        "global_code": "8FW4V9XM+29"
-                    },
-                    "types": [
-                        "establishment",
-                        "point_of_interest"
-                    ]
-                }
+                },
             ],
             "status": "OK"
         }
@@ -189,58 +109,62 @@ class TestFlaskApp:
 
         question_parsed_1 = "Openclassrooms"
         question_parsed_2 = "sqsdqqdqsdqsdq"
-        init_api = api_google.ApiGoogle()
+        question_parsed_3 = ""
 
-        get_api_address = init_api.get_address(question_parsed_1)
+        get_api_address = self.mock_api_google(question_parsed_1)
         print(get_api_address)
         assert get_api_address == example_true_reponse_api
 
-        get_api_address_2 = init_api.get_address(question_parsed_2)
+        get_api_address_2 = self.mock_api_google(question_parsed_2)
         print(get_api_address_2)
         assert get_api_address_2 == example_false_reponse_api
 
-    def test_get_api_wiki(self):
-        """
-        # TODO A FAIRE
-        :return:
-        """
+        get_api_address_3 = self.mock_api_google(question_parsed_3)
+        print(get_api_address_2)
+        assert get_api_address_2 == example_false_reponse_api
 
-        example_get = {
-            "batchcomplete": "",
-            "continue": {
-                "sroffset": 10,
-                "continue": "-||"
-            },
-            "query": {
-                "searchinfo": {
-                    "totalhits": 41
-                },
-                "search": [
-                    {
-                        "ns": 0,
-                        "title": "OpenClassrooms",
-                        "pageid": 4338589,
-                        "size": 30753,
-                        "wordcount": 3137,
-                        "snippet": "chez <span class=\"searchmatch\">OpenClassrooms</span> », <span class=\"searchmatch\">OpenClassrooms</span> : le blog,\u200e 17 avril 2018 (lire en ligne, consulté le 11 juillet 2018) « <span class=\"searchmatch\">OpenClassrooms</span> », sur <span class=\"searchmatch\">openclassrooms</span>.com",
-                        "timestamp": "2020-12-30T04:35:53Z"
-                    }
-                ]
-            }
-        }
-        example_get_no_found = {
-            "batchcomplete": "",
-            "query": {
-                "searchinfo": {
-                    "totalhits": 0
-                },
-                "search": []
-            }
-        }
-
-        init_wiki = wiki.Wiki()
-        get_test = init_wiki.get_wiki_address("Openclassrooms")
-        assert get_test == example_get
-
-        get_test_no_found = init_wiki.get_wiki_address("sqdqsdqsdqsdq")
-        assert get_test_no_found == example_get_no_found
+    # def test_get_api_wiki(self):
+    #     """
+    #     # TODO A FAIRE
+    #     :return:
+    #     """
+    #
+    #     example_get = {
+    #         "batchcomplete": "",
+    #         "continue": {
+    #             "sroffset": 10,
+    #             "continue": "-||"
+    #         },
+    #         "query": {
+    #             "searchinfo": {
+    #                 "totalhits": 41
+    #             },
+    #             "search": [
+    #                 {
+    #                     "ns": 0,
+    #                     "title": "OpenClassrooms",
+    #                     "pageid": 4338589,
+    #                     "size": 30753,
+    #                     "wordcount": 3137,
+    #                     "snippet": "chez <span class=\"searchmatch\">OpenClassrooms</span> », <span class=\"searchmatch\">OpenClassrooms</span> : le blog,\u200e 17 avril 2018 (lire en ligne, consulté le 11 juillet 2018) « <span class=\"searchmatch\">OpenClassrooms</span> », sur <span class=\"searchmatch\">openclassrooms</span>.com",
+    #                     "timestamp": "2020-12-30T04:35:53Z"
+    #                 }
+    #             ]
+    #         }
+    #     }
+    #     example_get_no_found = {
+    #         "batchcomplete": "",
+    #         "query": {
+    #             "searchinfo": {
+    #                 "totalhits": 0
+    #             },
+    #             "search": []
+    #         }
+    #     }
+    #
+    #     init_wiki = wiki.Wiki()
+    #     get_test = init_wiki.get_wiki_address("Openclassrooms")
+    #     assert get_test == example_get
+    #
+    #     get_test_no_found = init_wiki.get_wiki_address("sqdqsdqsdqsdq")
+    #     assert get_test_no_found == example_get_no_found
