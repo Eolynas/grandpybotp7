84f09ae2874b2889256de5884bf5e81593e3a9e1
Eddy <eddy.hubert@yahoo.fr>
2021-02-06 11:30:59 +0100

    add favicon

--- grandpy.py
+++ grandpy.py
@@ -3,5 +3,5 @@ import os
 from app import app
 
 if __name__ == "__main__":
-    debug = bool(os.environ.get('DEBUG'))
-    app.run(debug=True)
+    debug = bool(os.environ.get('DEBUG_FLASK'))
+    app.run(debug=debug)
